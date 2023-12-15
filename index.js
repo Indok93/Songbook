f5b872f1fcd80842407f686a7845596dc16f3756
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-12-15 12:10:24 +0100

    upd with mysql kald i routes

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 import { router as SongRouter} from './Routes/song.router.js';
 import { router as ContactRouter} from './Routes/contact.router.js';
 import { router as ArtistRouter} from './Routes/artist.router.js';
+import { router as songListRouter} from './Routes/songlist.router.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
+
 
 app.use(SongRouter);
+app.use(songListRouter);
 app.use(ContactRouter);
 app.use(ArtistRouter);
 
