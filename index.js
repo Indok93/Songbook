9aa2bc2542b7a8f39ab64d0edda30219ca89d556
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-12-15 11:06:49 +0100

    upd

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { router as SongRouter} from './Routes/song.router.js';
 import { router as ContactRouter} from './Routes/contact.router.js';
 import { router as ArtistRouter} from './Routes/artist.router.js';
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(SongRouter);
 app.use(ContactRouter);
 app.use(ArtistRouter);
 
-app.listen(4000, () => {
-    console.log('Server kører på port 4000');
+app.listen(port, () => {
+    console.log(`Server kører på port ${port}`);
 });
